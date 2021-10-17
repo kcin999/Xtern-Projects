b17dfb29cc2e036557ad2487b89a163de230b3e4
kcin999 <62564441+kcin999@users.noreply.github.com>
2021-10-16 22:36:11 -0400

    Eventbrite data to CSV

--- jsonToCSV.py
+++ jsonToCSV.py
@@ -1,6 +1,18 @@
 import pandas
+import json
 
+#Opens Raw JSON
+with open("raw_eventbrite.json", encoding='utf-8') as data_file:
+	data = json.load(data_file)
 
-df = pandas.read_json("raw_eventbrite.json",orient='values')
+#Takes JSON and brings it all to it's own level
+df = pandas.json_normalize(data,'results')
+
+#Takes care of escapte characters, like line-breaks.
+df['summary'] = df['summary'].str.replace("\\n","")
+
+#Keeps only the columns that I want
+df = df[['id','start_date','start_time','_type','end_date','end_time','primary_organizer_id','primary_organizer.name','name','summary','primary_venue.name','primary_venue.address.longitude','primary_venue.address.latitude','primary_venue.address.localized_address_display']]
 
 print(df)
+df.to_csv("eventbrite.csv",index=False)
